2e848082d02623da7261d10b23162fa94ba9844d
Igor Danish <danish_igor@mail.ru>
2019-12-06 15:13:42 +0400

    Добавление проекта в git.

---
+++ ReadMe.md
@@ -0,0 +1,48 @@
+TimeToken
+-----------------------
+
+Temporary tokens are often used in links to confirm E-mail and other actions.
+
+```php
+<?php
+
+use DanishIgor\TimeToken\Token;
+
+// Create TimeToken object.
+$tokenManager = new Token();
+
+// Generate token.
+$token = $tokenManager->generate(); // "c4NUUjEFp5eUZO9GyIdo_4204983745".
+
+// Check token.
+if ($tokenManager->check($token)) {
+    echo 'Token is correct.';
+} else {
+    echo 'Token is wrong.';
+}
+```
+
+By default, this time is 3600 seconds and the length is 32 lowercase characters. If necessary, in the constructor,
+you can specify the lifetime, length and set of characters from which a random string is generated.
+
+```php
+<?php
+
+use DanishIgor\TimeToken\Token;
+
+// Create TimeToken object with non-standard parameters.
+// Life time: 600 seconds.
+// Length token: 100 characters.
+// Characters for generation: 1, 2, 3, "a", "c", "x".
+$tokenManager = new Token(600, 100, [1, 2, 3, "a", "c", "x"]);
+
+// Generate token.
+$token = $tokenManager->generate(); // "1c2axx1x2_1575630395".
+
+// Check token.
+if ($tokenManager->check($token)) {
+    echo 'Token is correct.';
+} else {
+    echo 'Token is wrong.';
+}
+```
---
+++ src/Token.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace DanishIgor\TimeToken;
+
+/**
+ * Helper for working with temporary tokens.
+ *
+ * @package DanishIgor\TimeToken
+ */
+class Token
+{
+    /**
+     * @var integer $lifetime Token lifetime in seconds.
+     */
+    private $lifetime;
+    /**
+     * @var integer $length Length of the token along with the timestamp.
+     */
+    private $length;
+    /**
+     * @var array $characters Symbols from which the token is generated.
+     */
+    private $characters;
+
+    /**
+     * Constructor.
+     *
+     * @param integer $lifetime   Token lifetime in seconds.
+     * @param integer $length     Length of the token along with the timestamp.
+     * @param array   $characters Symbols from which the token is generated.
+     *
+     * @throws \Exception
+     */
+    public function __construct(
+        $lifetime = 3600,
+        $length = 32,
+        $characters = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r',
+            's', 't', 'u', 'v', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        ]
+    )
+    {
+        if (!is_integer($lifetime)) {
+            throw new \Exception('The value of "$lifetime" is not an integer.');
+        }
+
+        if (!is_integer($length)) {
+            throw new \Exception('The value of "$length" is not an integer.');
+        }
+
+        if (!is_array($characters)) {
+            throw new \Exception('The value of "$characters" is not an array.');
+        }
+
+        if ($lifetime < 1) {
+            throw new \Exception('Life time is too short.');
+        }
+
+        // The minimum length must consist of one or more random characters, a lower slash, and a timestamp.
+        if ($length < 2 + strlen(time())) {
+            throw new \Exception('The specified length is too short.');
+        }
+
+        if (count($characters) < 1) {
+            throw new \Exception('The array of characters to generate is empty.');
+        }
+
+        $this->lifetime = $lifetime;
+        $this->length = $length;
+        $this->characters = $characters;
+    }
+
+    /**
+     * Generate.
+     *
+     * @return string
+     */
+    public function generate()
+    {
+        $time = time();
+
+        return $this->generateRandomString($this->length - strlen($time) - 1) . '_' . $time;
+    }
+
+    /**
+     * Check.
+     *
+     * @param null|string $token Token.
+     *
+     * @return bool
+     */
+    public function check($token = null)
+    {
+        if (empty($token)) {
+            return false;
+        }
+
+        $bottomSlashPosition = strrpos($token, '_');
+
+        if ($bottomSlashPosition === false) {
+            return false;
+        }
+
+        return intval(substr($token, $bottomSlashPosition + 1)) + $this->lifetime >= time();
+    }
+
+    /**
+     * Generate string.
+     *
+     * @param integer $length Generated string length.
+     *
+     * @return string
+     **/
+    private function generateRandomString($length)
+    {
+        $result = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $index = rand(0, count($this->characters) - 1);
+            $result = $result . $this->characters[$index];
+        }
+
+        return $result;
+    }
+}
